d7859181332f64a8bdadff402ae9dc51732df752
Ash <ashy@tyty.cloud>
2024-05-12 18:37:54 +0800

    Initial commit

---
+++ Controllers/ShoeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoeSalesAPI.Models;
+using ShoeSalesAPI.Services;
+
+namespace ShoeSalesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShoeController : Controller
+    {
+        private readonly ShoeService _shoeService;
+
+        public ShoeController(ShoeService shoeService) =>
+            _shoeService = shoeService;
+
+
+        #region Get Requests
+        // Finds all products
+        [Route("allproducts")]
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var allProducts = await _shoeService.GetAllProducts();
+
+            if (allProducts == null)
+            {
+                return NotFound();
+            }
+            return Ok(allProducts);
+
+        }
+
+        // Filters shoes by their prices, with min and maximum
+        [Route("filterprice")]
+        [HttpGet]
+        public async Task<IActionResult> GetPrice(double minPrice, double maxPrice)
+        {
+            var rangeProducts = await _shoeService.GetShoesByPrice(minPrice, maxPrice);
+
+            if (rangeProducts == null)
+            {
+                return NotFound();
+            }
+            return Ok(rangeProducts);
+
+        }
+
+        // Finds all availible stock and displays it
+        [Route("isAvailable={available}")]
+        [HttpGet]
+        public async Task<IActionResult> GetStock(bool available)
+        {
+            var stock = await _shoeService.GetShoesByAvailibility(available);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+        #endregion
+
+        [Route("sku={sku}")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(int sku, Shoe updatedShoe)
+        {
+            var product = await _shoeService.UpdateProduct(sku, updatedShoe);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+
+    }
+}
---
+++ Services/ShoeService.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using ShoeSalesAPI.Models;
+
+namespace ShoeSalesAPI.Services
+{
+    public class ShoeService
+    {
+        private readonly IMongoCollection<Shoe> _shoeCollection;
+
+        public ShoeService(
+            IOptions<ShoeStoreDatabaseSettings> shoeStoreDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                shoeStoreDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                shoeStoreDatabaseSettings.Value.DatabaseName);
+
+            _shoeCollection = mongoDatabase.GetCollection<Shoe>(
+                shoeStoreDatabaseSettings.Value.ShoesCollectionName);
+        }
+        /// <summary>
+        /// Fetches all products based on a mongo query async task
+        /// </summary>
+        /// <returns>async list of all products</returns>
+        public async Task<List<Shoe>> GetAllProducts()
+        {
+            return await _shoeCollection.Find(_ => true).ToListAsync();
+        }
+
+        /// <summary>
+        /// Fetches price range based on a mongo query async task
+        /// </summary>
+        /// <param name="minPrice">Minimum price of the product filter</param>
+        /// <param name="maxPrice">Maximum price of the product filter</param>
+        /// <returns>async list of all products within the price range</returns>
+        public async Task<List<Shoe>> GetShoesByPrice(double minPrice, double maxPrice)
+        {
+            return await _shoeCollection.Find(shoe => minPrice <= shoe.Price && shoe.Price <= maxPrice).ToListAsync();
+        }
+
+        /// <summary>
+        /// Fetches only availible stock based on mongo query async task
+        /// </summary>
+        /// <returns>async list of all products that are currently in stock</returns>
+        public async Task<List<Shoe>> GetShoesByAvailibility(bool available)
+        {
+            return await _shoeCollection.Find(shoe => shoe.isAvailable == available).ToListAsync();
+        }
+
+
+        public async Task<Shoe> UpdateProduct(int sku, Shoe updatedShoe)
+        {
+            var filter = Builders<Shoe>.Filter.Eq(s => s.SKU, sku);
+
+            var update = Builders<Shoe>.Update.Set(s => s.ProductName, updatedShoe.ProductName)
+                                             .Set(s => s.Price, updatedShoe.Price)
+                                             .Set(s => s.isAvailable, updatedShoe.isAvailable)
+                                             .Set(s => s.Description, updatedShoe.Description);
+
+            // Update the shoe document in the collection
+            var updateResult = await _shoeCollection.UpdateOneAsync(filter, update);
+
+            var updatedProduct = await _shoeCollection.FindAsync(filter);
+            return updatedProduct.FirstOrDefault();
+        }
+
+
+
+    }
+}
